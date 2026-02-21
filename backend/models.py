0d89bba9abcd9466ad3e9399d042e191f454581b
shouq112311 <128868282+shouq112311@users.noreply.github.com>
2026-02-21 23:18:25 +0300

    Use environment variables for DB credentials

--- backend/models.py
+++ backend/models.py
@@ -1,13 +1,11 @@
-import pip
 
+import os
+from dotenv import load_dotenv
 from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
-database_name = 'trivia'
-database_user = 'postgres'
-database_password = 'Kkesh1234'
-database_host = 'localhost:5432'
-database_path = f'postgresql://{database_user}:{database_password}@{database_host}/{database_name}'
+load_dotenv()
 
+database_path = os.getenv("DATABASE_URL")
 db = SQLAlchemy()
 """
 setup_db(app)
